2c4213ac89fdc41e394d3552e40692b6fe865d4c
Kenny Wu <kw413@icloud.com>
2020-08-03 01:45:52 -0400

    added default blank page content

---
+++ wp-content/themes/lumieretheme/page.php
@@ -0,0 +1,12 @@
+<?php if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+    }
+} ?>
+<?php get_header(); ?>
+
+        <article class="emptyPage">
+            <?php the_content(); ?>
+        </article>
+
+<?php get_footer(); ?>
